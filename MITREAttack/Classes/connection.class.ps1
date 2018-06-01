a8f7d871763e859ed6a4f1706171e6c60802bfaf
John Bruckler <jpbruckler@gmail.com>
2018-06-01 16:40:30 -0400

    update connection class

--- MITREAttack/Classes/connection.class.ps1
+++ MITREAttack/Classes/connection.class.ps1
@@ -1,11 +1,24 @@
+enum MitreCollection
+{
+    EnterpriseAttack = 1
+    PreAttack        = 2
+    MobileAttack     = 3
+}
+
 class MitreConnection
 {
     [string] $StixBaseUrl      = "https://cti-taxii.mitre.org/taxii/"
     [string] $StixAcceptString = 'application/vnd.oasis.taxii+json; version=2.0'
     [Net.SecurityProtocolType] $TlsVersion
 
-    hidden [string] $ApiRoot
-    hidden [hashtable] $HttpHeader
+    [string] $ApiRoot
+    [string] $CollectionsRoot
+    [MitreCollection] $DefaultCollectionType = [MitreCollection]::EnterpriseAttack
+    [string] $DefaultCollectionId
+    [string] $DefaultCollectionUri
+    [hashtable] $DefaultCollectionHeader
+    [pscustomobject] $AllCollections
+    [hashtable] $HttpHeader
 
     MitreConnection()
     {
@@ -35,10 +48,22 @@ class MitreConnection
 
         $this.HttpHeader = @{ 'Accept' = $this.StixAcceptString }
         $this.ApiRoot = Invoke-RestMethod -Uri $this.StixBaseUrl -Headers $this.HttpHeader -Method Get | Select-Object -ExpandProperty api_roots
+        $this.CollectionsRoot = '{0}collections/' -f $this.ApiRoot
+        $this.GetCollections()
     }
 
-    GetCollections()
+    hidden GetCollections()
     {
+        $Response = Invoke-RestMethod -Uri $this.CollectionsRoot -Headers $this.HttpHeader -Method Get | Select-Object -ExpandProperty collections
+        $this.AllCollections = $Response
 
+        foreach ($Collection in $Response) {
+            $SearchTitle = ($Collection.Title -replace 'ATT&CK','Attack') -replace ' |\-',''
+            if ($SearchTitle -eq $this.DefaultCollectionType) {
+                $this.DefaultCollectionHeader   = @{ Accept = ($Collection | Select-Object -ExpandProperty media_types) }
+                $this.DefaultCollectionId       = $Collection.Id
+                $this.DefaultCollectionUri      = '{0}{1}' -f $this.CollectionsRoot,$this.DefaultCollectionId
+            }
+        }
     }
 }
